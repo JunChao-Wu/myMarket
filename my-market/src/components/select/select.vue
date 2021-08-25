9021271af6d155763576a56baf759c3f9a6bca38
JC <781462922@qq.com>
2021-08-25 23:13:51 +0800

    修改select组件bug以及添加select可清除选择功能

--- my-market/src/components/select/select.vue
+++ my-market/src/components/select/select.vue
@@ -3,23 +3,29 @@
     :class="{ select_show: focus} "
     class="select-container">
       <span
-        class="select-open-i iconfont icon-zhankai"
+        class="select-open-i iconfont icon-hidden"
+        :class="{'icon-zhankai': !choosedContent, 'icon-show': !choosedContent}"
         @click="handlerChange"></span>
+        <span
+        class="select-open-i iconfont icon-hidden"
+        :class="{'icon-cha': choosedContent, 'icon-show': choosedContent}"
+        @click="clearContent"></span>
       <input
         type="text"
-        v-model="choosedContent"
-        placeholder="请选择分类"
-        :style="selectInputStyle"
         class="select-input"
+        :style="selectInputStyle"
         @click="handlerChange">
-      <div class="text-container">{{ choosedContent }}</div>
+      <div
+        class="text-container"
+        :class="{'no-content': !choosedContent}"
+        @click="handlerChange">{{ choosedContent ? choosedContent : "请选择分类" }}</div>
       <ul
         :style="{width:width}"
         class="options-block">
         <li
           v-for="obj in optionList"
           :key="obj.value"
-          :value="obj.value"
+          :data-value="obj.value"
           class="option-cell"
           @click="optionChoosed">
             {{ obj.label }}
@@ -89,10 +95,15 @@ export default {
     handlerChange() {
       this.focus = !this.focus;
     },
+    clearContent() {
+      this.focus = false;
+      this.selectValue = null;
+      this.choosedContent = ''
+    },
     optionChoosed(e) {
       this.focus = !this.focus;
       this.choosedContent = e.target.textContent;
-      this.selectValue = e.target.value;
+      this.selectValue = e.target.getAttribute('data-value');
     }
   },
   data() {
@@ -109,9 +120,11 @@ export default {
 
 <style lang="less" scoped>
 @import '../../assets/css/common.less';
-
+.icon-hidden {
+  visibility: hidden;
+}
 .select_show .select-open-i {
-  transform: rotateX(180deg);
+  transform: rotate(-180deg);
 }
 .select_show .options-block {
   -webkit-transform: scaleY(1) !important;
@@ -120,10 +133,14 @@ export default {
 
 
 .select-container {
+  line-height: 30px;
   position: relative;
   width: 187px;
   display: inline-block;
   box-sizing: border-box;
+  .icon-show {
+    visibility: visible;
+  }
   .select-open-i {
     color: @basegray;
     cursor: pointer;
@@ -140,12 +157,17 @@ export default {
     padding: 4px 0 4px 10px;
     color: transparent;
     text-shadow: 0 -1 0 #000;
+    .no-touch();
   }
   .text-container {
     position: absolute;
     top: 0;
     left: 0;
+    cursor: pointer;
     padding-left: 10px;
+  }
+  .no-content {
+    color: @lightgray;
   }
   .options-block {
     z-index: 2;
